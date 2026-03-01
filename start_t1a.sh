d2e358dc2c21d62bfc956c198928998f9b3b9031
Mema <mema@glassgallery.my.id>
2026-03-01 22:30:10 +0000

    chore: remove obj folder and startup script from git tracking

--- start_t1a.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/.openclaw/workspace/projects/noclaw
-export NOCLAW_TELEGRAM_TOKEN="8608842627:AAE1utGnW2ZoD6PsOypkLjX3nD1jRJCVpMI"
-while true; do
-    ./noclaw agent --channel telegram >> /tmp/t1a_telegram.log 2>&1
-    sleep 5
-done
